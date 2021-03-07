1da65f88e3f28b3fbbed8ed34ff1ada1ec0bb3ff
Andreas Smas <andreas@lonelycoder.com>
2021-03-07 13:28:38 +0100

    libav: Add libavformat

--- libav.mk
+++ libav.mk
@@ -7,8 +7,8 @@ ifeq '$(shell $(PKG_CONFIG) 2>/dev/null libavcodec ; echo $$?)' '0'
 
 HAVE_LIBAV := yes
 
-LIBAV_LDFLAGS  += $(shell $(PKG_CONFIG) --libs libavcodec libavutil)
-LIBAV_CPPFLAGS += $(shell $(PKG_CONFIG) --cflags libavcodec libavutil)
+LIBAV_LDFLAGS  += $(shell $(PKG_CONFIG) --libs libavformat libavcodec libavutil)
+LIBAV_CPPFLAGS += $(shell $(PKG_CONFIG) --cflags libavformat libavcodec libavutil)
 
 endif
 
