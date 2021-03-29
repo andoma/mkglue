393da4c359c5475d83a0d6bcf0c0429353efd378
Andreas Smas <andreas@lonelycoder.com>
2021-03-29 20:08:31 +0200

    Add glfw3

---
+++ glfw3.mk
@@ -0,0 +1,18 @@
+PKG_CONFIG ?= pkg-config
+
+ifndef GLFW3_MK
+LGFW3_MK := 1
+
+ALLDEPS += mkglue/glfw3.mk
+
+ifeq ($(shell uname),Linux)
+
+GLFW3_PKGS := glew glfw3
+CPPFLAGS += $(shell pkg-config ${GLFW3_PKGS} --cflags)
+LDFLAGS  += $(shell pkg-config ${GLFW3_PKGS} --libs)
+
+endif
+
+
+
+endif
