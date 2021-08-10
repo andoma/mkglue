7150a0bc74a2401908022a5b4d4e1abb632f4194
Andreas Smas <andreas@lonelycoder.com>
2021-08-10 12:31:08 +0200

    glfw: Use glew on macos

--- glfw3.mk
+++ glfw3.mk
@@ -5,20 +5,16 @@ LGFW3_MK := 1
 
 ALLDEPS += mkglue/glfw3.mk
 
-ifeq ($(shell uname),Linux)
 
-GLFW3_PKGS := glew glfw3
-CPPFLAGS += $(shell pkg-config ${GLFW3_PKGS} --cflags)
-LDFLAGS  += $(shell pkg-config ${GLFW3_PKGS} --libs)
-
-else ifeq ($(shell uname),Darwin)
-
-GLFW3_PKGS := glfw3
-CPPFLAGS += $(shell pkg-config ${GLFW3_PKGS} --cflags)
-LDFLAGS  += $(shell pkg-config ${GLFW3_PKGS} --libs)
+ifeq ($(shell uname),Darwin)
 
 LDFLAGS += -framework OpenGL
 
 endif
+
+GLFW3_PKGS := glew glfw3
+
+CPPFLAGS += $(shell pkg-config ${GLFW3_PKGS} --cflags)
+LDFLAGS  += $(shell pkg-config ${GLFW3_PKGS} --libs)
 
 endif
