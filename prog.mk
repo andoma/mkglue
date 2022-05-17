ec0f27711bd76c736641a96532d616e0308cb6ca
Andreas Smas <andreas@lonelycoder.com>
2022-05-17 14:45:33 +0200

    Add support for compiling glsl files (into c arrays)

--- prog.mk
+++ prog.mk
@@ -2,9 +2,10 @@ ALLDEPS += mkglue/prog.mk
 
 SRCS += ${SRCS-yes}
 
-OBJS += $(patsubst %.c,   $(O)/%.o, $(filter %.c,   ${SRCS}))
-OBJS += $(patsubst %.cc,  $(O)/%.o, $(filter %.cc,  ${SRCS}))
-OBJS += $(patsubst %.cpp, $(O)/%.o, $(filter %.cpp, ${SRCS}))
+OBJS += $(patsubst %.c,    $(O)/%.o,      $(filter %.c,   ${SRCS}))
+OBJS += $(patsubst %.cc,   $(O)/%.o,      $(filter %.cc,  ${SRCS}))
+OBJS += $(patsubst %.cpp,  $(O)/%.o,      $(filter %.cpp, ${SRCS}))
+OBJS += $(patsubst %.glsl, $(O)/%.glsl.o, $(filter %.glsl, ${SRCS}))
 
 DEPS +=  ${OBJS:%.o=%.d}
 
@@ -27,6 +28,16 @@ ${O}/%.o: %.cpp $(ALLDEPS) $(SRCDEPS)
 	@mkdir -p $(dir $@)
 	@echo "\tCXX\t$@"
 	$(CXX) -MD -MP $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $(CURDIR)/$<
+
+${O}/%.glsl.c: %.glsl ${ALLDEPS}
+	@mkdir -p $(dir $@)
+	@echo "\tXXD\t$@"
+	@(cd $(dir $<) && xxd -i $(notdir $<)) >$@
+
+${O}/%.glsl.o: ${O}/%.glsl.c ${ALLDEPS}
+	@mkdir -p $(dir $@)
+	@echo "\tCC\t$@"
+	$(CC) -MD -MP $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
 
 clean:
 	rm -rf "${O}"
