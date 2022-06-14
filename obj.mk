c55053d4080ece8bfca470bcef105fe9de645fc3
Andreas Smas <andreas@lonelycoder.com>
2022-06-14 10:16:23 +0200

    Add support for transforming PNG files into .o files

--- obj.mk
+++ obj.mk
@@ -5,7 +5,8 @@ SRCS += ${SRCS-yes}
 OBJS += $(patsubst %.c,    $(O)/%.o,      $(filter %.c,   ${SRCS}))
 OBJS += $(patsubst %.cc,   $(O)/%.o,      $(filter %.cc,  ${SRCS}))
 OBJS += $(patsubst %.cpp,  $(O)/%.o,      $(filter %.cpp, ${SRCS}))
-OBJS += $(patsubst %.glsl, $(O)/%.glsl.o, $(filter %.glsl, ${SRCS}))
+OBJS += $(patsubst %.glsl, $(O)/%.glsl.o, $(filter %.glsl,${SRCS}))
+OBJS += $(patsubst %.png,  $(O)/%.png.o,  $(filter %.png, ${SRCS}))
 
 DEPS +=  ${OBJS:%.o=%.d}
 
@@ -30,6 +31,16 @@ ${O}/%.glsl.c: %.glsl ${ALLDEPS}
 	@(cd $(dir $<) && xxd -i $(notdir $<)) >$@
 
 ${O}/%.glsl.o: ${O}/%.glsl.c ${ALLDEPS}
+	@mkdir -p $(dir $@)
+	@echo "\tCC\t$@"
+	$(CC) -MD -MP $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+
+${O}/%.png.c: %.png ${ALLDEPS}
+	@mkdir -p $(dir $@)
+	@echo "\tXXD\t$@"
+	@(cd $(dir $<) && xxd -i $(notdir $<)) >$@
+
+${O}/%.png.o: ${O}/%.png.c ${ALLDEPS}
 	@mkdir -p $(dir $@)
 	@echo "\tCC\t$@"
 	$(CC) -MD -MP $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
